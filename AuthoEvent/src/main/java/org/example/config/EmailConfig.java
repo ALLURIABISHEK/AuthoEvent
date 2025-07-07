933088def93071d92c56dbaa7dfc638b11aff902
ALLURI ABISHEK KUMAR <aaarts45516@gmail.com>
2025-07-07 11:08:15 +0530

    Add files via upload

---
+++ AuthoEvent/src/main/java/org/example/config/EmailConfig.java
@@ -0,0 +1,9 @@
+package org.example.config;
+
+public class EmailConfig {
+    public static final String SMTP_HOST = "smtp.gmail.com";
+    public static final String SMTP_PORT = "587";
+    public static final String USERNAME = "abiprojectsworking@gmail.com";
+    public static final String PASSWORD = "sctnrmurilgzdjww";  // App Password (no spaces)
+    public static final String FROM = "abiprojectsworking@gmail.com";
+}
